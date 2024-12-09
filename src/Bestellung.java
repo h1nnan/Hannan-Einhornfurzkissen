ab64eabdfc534646585d40d6c2869db79bf8691c
buttha <hannanalibutt10@gmail.com>
2024-12-09 09:34:37 +0100

    ShopServer

---
+++ src/Bestellung.java
@@ -0,0 +1,24 @@
+public class Bestellung {
+    private boolean besteatigt;
+    private int indr;
+
+    Bestellung(Einhornfurzkissen pWare){
+
+    }
+
+    public boolean getBesteatigt() {
+        return besteatigt;
+    }
+
+    public void setBesteatigt(boolean besteatigt) {
+        this.besteatigt = besteatigt;
+    }
+
+    public int getIndr() {
+        return indr;
+    }
+
+    public void setIndr(int indr) {
+        this.indr = indr;
+    }
+}
---
+++ src/Einhornfurzkissen.java
@@ -0,0 +1,32 @@
+public class Einhornfurzkissen {
+    private String groesse;
+    private String farbe;
+    private double preis;
+
+    public Einhornfurzkissen() {
+    }
+
+    public String getGroesse() {
+        return groesse;
+    }
+
+    public void setGroesse(String groesse) {
+        this.groesse = groesse;
+    }
+
+    public String getFarbe() {
+        return farbe;
+    }
+
+    public void setFarbe(String farbe) {
+        this.farbe = farbe;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
+
+    public void setPreis(double preis) {
+        this.preis = preis;
+    }
+}
