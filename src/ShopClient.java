659e0ab747ec90e10a60d0cb5a468784655270c3
buttha <hannanalibutt10@gmail.com>
2024-12-13 09:29:28 +0100

    update

--- src/ShopClient.java
+++ src/ShopClient.java
@@ -1,2 +1,20 @@
-public class ShopClient {
+import Utills.Client;
+
+public class ShopClient extends Client {
+
+    ShopClient(String pServerIP, int pServerPort) {
+        super(pServerIP, pServerPort);
+    }
+
+    public void groesse FarbeWaehlen(String pGroesse, String pFarbe){
+        this.send("Einhornfurzkissen: "+ pGroesse+":"+pFarbe);
+    }
+
+    public void bestaetigen(String pAntwort){
+        this.send ("BESTAETIGUNG:"+ pAntwort);
+    }
+
+    public void abmelden() {
+        this.send("ABMELDEN");
+    }
 }
